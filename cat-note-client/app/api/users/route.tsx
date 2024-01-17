d81b7b4ed1948537a9346b128c4be1fbc5750359
Veronika Pristromskaya <veronika.pr777@gmail.com>
2024-01-17 12:19:38 +0100

    delete message

--- cat-note-client/app/api/users/route.tsx
+++ cat-note-client/app/api/users/route.tsx
@@ -4,21 +4,20 @@ import {NextResponse} from "next/server";
 const backendApi = process.env.NEXT_PUBLIC_API_URL;
 
 export async function POST(req: any) {
-  // const {accessToken} = await getAccessToken(req);
-  // const body = await req.json();
+  const {accessToken} = await getAccessToken(req);
+  const body = await req.json();
 
-  // const response = await fetch(`${backendApi}/api/user/${body.userName}`,
-  //   {
-  //     method: 'POST',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //       Authorization: `Bearer ${accessToken}`
-  //     }
-  //   });
+  const response = await fetch(`${backendApi}/api/user/${body.userName}`,
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
 
-  // const responseJson = await response.json();
-  // return NextResponse.json(responseJson);
-  return NextResponse.json({message: "smth"})
+  const responseJson = await response.json();
+  return NextResponse.json(responseJson);
 }
 
 export async function PUT(req: any){
